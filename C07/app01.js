4f588dd356565845acd64ab9b0158dc3cbdd3390
Chao <zhuchao8931@vip.qq.com>
2017-01-02 10:01:13 +0800

    filter过滤器

--- C07/app01.js
+++ C07/app01.js
@@ -5,4 +5,7 @@
 var app = angular.module("myApp", []);
 app.controller("MyController", function ($scope) {
     $scope.today = "2014-12-01";
+    $scope.isCapitalized=function (str) {
+        return str[0]==str[0].toUpperCase();
+    }
 })
